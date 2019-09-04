88a5abed98eb1b18e0a17950e1b95e5891cc739e
Aleksa-Jovanovicu <aleksa.j.1996@gmail.com>
2019-09-05 00:36:23 +0200

    Pushing some fixes

--- resources/js/ytd/src/app/shared/YoutubeVideo.ts
+++ resources/js/ytd/src/app/shared/YoutubeVideo.ts
@@ -2,14 +2,14 @@ import * as urlParse from 'url-parse';
 
 export class YoutubeVideo {
 
-    constructor(private id,
-        private title,
-        private videoId,
-        private lengthSeconds,
-        private thumbnail,
-        private requested,
-        private lastRequest,
-        private status,
+    constructor(public id,
+        public title,
+        public videoId,
+        public lengthSeconds,
+        public thumbnail,
+        public requested,
+        public lastRequest,
+        public status,
     ) { }
 
     public static getVideoIdByLink(link: string): string {
--- resources/js/ytd/src/environments/environment.prod.ts
+++ resources/js/ytd/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://ec2-54-213-155-126.us-west-2.compute.amazonaws.com'
 };
