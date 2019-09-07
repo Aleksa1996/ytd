0a3f4a16d9f685cca21598c405a24907fe232eb8
Aleksa-Jovanovicu <aleksa.j.1996@gmail.com>
2019-09-07 10:37:23 +0200

    change api url in enviroment

--- resources/js/ytd/src/environments/environment.prod.ts
+++ resources/js/ytd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-213-155-126.us-west-2.compute.amazonaws.com',
+  apiUrl: 'https://ytd.aleksajovanovic.com',
   backendUrl: 'http://nginx'
 };
